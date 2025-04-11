b4f9f1903ff0445a15b1d8727d780b9a53185ff3
Brandon Manogura <brandonmanogura@Brandons-MacBook-Air.local>
2025-04-10 19:16:54 -0500

    project 1 checkpoint

--- flaskapp.py
+++ flaskapp.py
@@ -2,10 +2,7 @@
 # description: Flask example using redirect, url_for, and flash
 # credit: the template html files were constructed with the help of ChatGPT
 
-from flask import Flask
-from flask import render_template
-from flask import Flask, render_template, request, redirect, url_for, flash
-
+'''
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
                                    # it is required, but you can leave this alone
@@ -54,6 +51,60 @@ def delete_users():
         return redirect(url_for('home'))
     else:
         return render_template('delete_user.html')
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+'''
+
+
+
+'''
+@app. route("/dashboard", methods= ["GET", "POST"])
+def dashboard():
+    if request.method == "POST":
+#a Form submission with profile data
+        username = request. form. get ("username")
+        first_name = request. form.get ("first _name")
+        selected_languages = request. form.getlist ("languages") # List of selected languages
+# SOL query to get countries where those languages are spoken
+        if selected_languages:
+            country_matches = get_countries_by_languages(selected_languages)
+        else:
+            country_matches = []
+# Save first name and selected languages to user's DynamoDB profile
+        update_user_profile (username, first_name, ", "join(selected_languages))
+# Re-render dashboard with results
+        return render_template(
+            "dashboard.html",
+            success=True, 
+            name=first_name, 
+            languages=", "•join(selected_languages), 
+            country_matches=country_matches
+# GET request: show form for entering name and selecting languages.
+username = request.args.get ("username")
+languages = get_ languages ()
+# Read from query string
+# Get full list of languages from MySQL
+return render_template("dashboard:html", success-False, username-username, languages=languages)
+'''
+#---------------------------------------------------------------
+#
+from flask import Flask
+#from flask import render_templates
+from flask import Flask, render_template, request, redirect, url_for, flash
+from dbCode import *
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    #Query the top 10 countries from the MySQL
+    countries = get_list_of_dictionaries()
+    #Render the index page with the list of countries
+    return render_template("index.html", results=countries)
+
+
+
+
 # these two lines of code should always be the last in the file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
